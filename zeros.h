026691519159027ca376959d38c55066400ce12d
afm97 <arthurfranca97@gmail.com>
2023-11-27 19:02:13 -0300

    printMatrix.h
    Responsavel por imprimir uma matriz

---
+++ zeros.h
@@ -0,0 +1,11 @@
+float zeros(unsigned int rows, unsigned int cols, double matrix[rows][cols])
+{
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i][j] = 0.0;
+        }
+    }
+}
