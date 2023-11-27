161ae1892ab66006bf2f4890452293267eeb1faa
afm97 <arthurfranca97@gmail.com>
2023-11-27 19:34:40 -0300

    Remocao da parte de substituicoes regressivas e adicionada compatibilidade para trabalho com matriz aumentada

--- gaussianElimination.h
+++ gaussianElimination.h
@@ -1,46 +1,21 @@
 
-
-void gaussianElimination(int rows, float matrix[rows][rows], float solution[rows][3])
+void gaussianElimination(int rows, double matrix[rows][rows + 1])
 {
 
-	float ratio = 0, sol_aux;
+	double pivot = 0;
 
-	/* Aplicando a Eliminação de Gauss */
 	for (int i = 0; i < rows - 1; i++)
 	{
-
-		if (matrix[i][i] == 0.0)
-		{
-
-			printf("Mathematical Error!");
-			exit(0);
-		}
 		for (int j = i + 1; j < rows; j++)
 		{
 
-			ratio = matrix[j][i] / matrix[i][i];
+			pivot = matrix[j][i] / matrix[i][i];
 
-			for (int k = 0; k < rows; k++)
+			for (int k = 0; k < rows + 1; k++)
 			{
 
-				matrix[j][k] = matrix[j][k] - ratio * matrix[i][k];
+				matrix[j][k] = matrix[j][k] - pivot * matrix[i][k];
 			}
-			solution[j][1] = solution[j][1] - ratio * solution[i][1];
 		}
-	}
-
-	/* Obtendo a solução do sistema linear */
-
-	solution[rows - 1][1] = solution[rows - 1][1] / matrix[rows - 1][rows - 1];
-
-	for (int l = rows - 2; l >= 0; l--)
-	{
-		sol_aux = 0;
-		for (int k = rows - 1; k > l; k--)
-		{
-
-			sol_aux = sol_aux + matrix[l][k] * solution[k][1];
-		}
-		solution[l][1] = (solution[l][1] - sol_aux) / matrix[l][l];
 	}
 }
