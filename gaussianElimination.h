e79b7f5b06fb7fcfd7dc590742103a886cb39f59
afm97 <arthurfranca97@gmail.com>
2023-11-29 17:59:09 -0300

    Implementacao do pivotamento parcial no metodo de eliminacao de gauss

--- gaussianElimination.h
+++ gaussianElimination.h
@@ -1,17 +1,21 @@
+void partialPivotingScale(unsigned originRow, unsigned int numberOfRows, double matrix[numberOfRows][numberOfRows + 1]);
 
-void gaussianElimination(unsigned int rows, double matrix[rows][rows + 1])
+void gaussianElimination(unsigned int numberOfRows, double matrix[numberOfRows][numberOfRows + 1])
 {
+	double copiedMatrix[numberOfRows][numberOfRows];
 
 	double pivot = 0;
 
-	for (int i = 0; i < rows - 1; i++)
+	for (int i = 0; i < numberOfRows - 1; i++)
 	{
-		for (int j = i + 1; j < rows; j++)
+
+		for (int j = i + 1; j < numberOfRows; j++)
 		{
+			partialPivotingScale(i, numberOfRows, matrix);
 
 			pivot = matrix[j][i] / matrix[i][i];
 
-			for (int k = 0; k < rows + 1; k++)
+			for (int k = 0; k < numberOfRows + 1; k++)
 			{
 
 				matrix[j][k] = matrix[j][k] - pivot * matrix[i][k];
