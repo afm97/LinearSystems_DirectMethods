3f93b8a0e3732b92f78b0819b4e0601594372d88
afm97 <arthurfranca97@gmail.com>
2023-11-29 18:06:48 -0300

    Insercao das bibliotecas e finalizacao do programa

--- main.c
+++ main.c
@@ -4,18 +4,31 @@
 #include "zeros.h"
 #include "printMatrix.h"
 #include "gaussianElimination.h"
+#include "regressiveSubstitution.h"
+#include "rowExchanger.h"
+#include "maxValueOfLine.h"
+#include "partialPivotingScale.h"
 
 int main()
 {
 
-    float data[][3] = {{0, -1, 1},
-                       {2.5, 5.95915546, 0},
-                       {5, 19.80464235, 0},
-                       {7.5, 49.55234065, 0},
-                       {10, 103.08082062, 2.14917705}};
+    double matrix[][5] = {{1.19, 2.11, -100, 1, 1.12},
+                          {14.2, -0.122, 12.2, -1, 3.44},
+                          {0, 100, -99, 1, 2.15},
+                          {15.3, 0.110, -13.1, -1, 4.16}};
 
-    int rows = sizeof(data) / sizeof(data[0]);
-    int cols = sizeof(data[0]) / sizeof(data[0][0]);
+    // sol:{x1 = 0.716, x2 = 0.0126, x3 = -0.0206, x4 = -1.18}
 
-    float constants[rows][3];
+    int rows = sizeof(matrix) / sizeof(matrix[0]);
+    int cols = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+    double solution[rows][1];
+
+    printMatrix(rows, cols, matrix);
+    gaussianElimination(rows, matrix);
+    printf("---------------------------------------------------------------\n");
+    printMatrix(rows, cols, matrix);
+    regressiveSubstitution(rows, matrix, solution);
+    printf("---------------------------------------------------------------\n");
+    printMatrix(rows, 1, solution);
 }
