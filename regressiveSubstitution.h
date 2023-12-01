4d433cd4149571ed545b7e55b48cb22eec5fe370
afm97 <arthurfranca97@gmail.com>
2023-12-01 17:00:04 -0300

    Alteracao da matriz coluna Solution para array solution

--- regressiveSubstitution.h
+++ regressiveSubstitution.h
@@ -1,19 +1,19 @@
 
-void regressiveSubstitution(unsigned int rows, double matrix[rows][rows + 1], double solution[rows][1])
+void regressiveSubstitution(unsigned int rows, double matrix[rows][rows + 1], double solution[rows])
 {
     double sol_aux = 0;
 
-    solution[rows - 1][0] = matrix[rows - 1][rows] / matrix[rows - 1][rows - 1]; // Primeira solução é manual e restante via somatório
+    solution[rows - 1] = matrix[rows - 1][rows] / matrix[rows - 1][rows - 1]; // Primeira solução é manual e restante via somatório
 
     for (int i = rows - 2; i >= 0; i--)
     {
         for (int j = rows - 1; j > i; j--)
         {
 
-            sol_aux += matrix[i][j] * solution[j][0];
+            sol_aux += matrix[i][j] * solution[j];
         }
 
-        solution[i][0] = (matrix[i][rows] - sol_aux) / matrix[i][i];
+        solution[i] = (matrix[i][rows] - sol_aux) / matrix[i][i];
 
         sol_aux = 0;
     }
