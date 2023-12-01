08a589199342767bc93b9eb07ba1a711322234d6
afm97 <arthurfranca97@gmail.com>
2023-12-01 16:59:01 -0300

    Alteracao da posicao do pivotamento e implementado nova metodologia para pivotar a matriz

--- gaussianElimination.h
+++ gaussianElimination.h
@@ -1,18 +1,20 @@
-void partialPivotingScale(unsigned originRow, unsigned int numberOfRows, double matrix[numberOfRows][numberOfRows + 1]);
+void maxValuesOfMatrix(unsigned int numberOfRows, double matrix[numberOfRows][numberOfRows + 1], double arrayOfMaxValues[numberOfRows]);
+void partialPivotingScale(unsigned originRow, unsigned int numberOfRows, double matrix[numberOfRows][numberOfRows + 1], double maxValuesOfMatrix[numberOfRows]);
 
 void gaussianElimination(unsigned int numberOfRows, double matrix[numberOfRows][numberOfRows + 1])
 {
-	double copiedMatrix[numberOfRows][numberOfRows];
+
+	double arrayOfMaxValues[numberOfRows];
+	maxValuesOfMatrix(numberOfRows, matrix, arrayOfMaxValues);
 
 	double pivot = 0;
 
 	for (int i = 0; i < numberOfRows - 1; i++)
 	{
+		partialPivotingScale(i, numberOfRows, matrix, arrayOfMaxValues);
 
 		for (int j = i + 1; j < numberOfRows; j++)
 		{
-			partialPivotingScale(i, numberOfRows, matrix);
-
 			pivot = matrix[j][i] / matrix[i][i];
 
 			for (int k = 0; k < numberOfRows + 1; k++)
